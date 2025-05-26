02004ac3499134cf0b12140541390aec22f6af7c
kongming <kongming434@gamil.com>
2025-05-26 16:58:16 +0700

    new

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -5,7 +5,7 @@
 
     <!-- Hero Section -->
     <section class="relative bg-cover bg-center w-full h-[70vh] bg-no-repeat mt-5"
-        style="background-image: url('{{ url('images/slide1.svg') }}');">
+        style="background-image: url('{{ asset('/images/slide1.svg') }}');">
         <div class="relative z-10 flex items-center h-full ml-36">
             <a href="#explore"
                 class="px-10 py-5 bg-slate-950 text-3xl font-bold rounded-full shadow-lg hover:bg-slate-500 hover:scale-105 transition-all duration-300 text-white block text-center">
@@ -47,7 +47,7 @@
                     href="{{ route('productSort', ['type' => 'brand','brand'=>$brand['id']]) }}">
                     <div
                         class="bg-white flex flex-col justify-center items-center py-3 shadow-lg hover:shadow-2xl transition duration-300">
-                        <img src="{{ url('/' . $brand['image']) }}" alt="{{ $brand['brand_name'] }}"
+                        <img src="{{ asset('/' . $brand['image']) }}" alt="{{ $brand['brand_name'] }}"
                             class="w-96 h-40 object-contain ">
                     </div>
                 </a>
