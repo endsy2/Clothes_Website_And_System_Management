318349fe77eed6c3f9dce84072a9dc5e737baf98
kongming <kongming434@gamil.com>
2025-05-30 16:22:11 +0700

    new

--- config/database.php
+++ config/database.php
@@ -94,7 +94,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
 
