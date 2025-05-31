881ffa31321b2bc9757283a6cb299da9d7c91606
kongming <kongming434@gamil.com>
2025-05-31 14:00:22 +0700

    new

--- config/database.php
+++ config/database.php
@@ -82,18 +82,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver'   => 'pgsql',
-            'host'     => env('DB_HOST'),
-            'port'     => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD',),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-            'sslmode'  => 'require',
-        ],
+
 
 
 
