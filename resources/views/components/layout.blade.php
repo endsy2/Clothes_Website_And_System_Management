32bbd77ccea4e52151197e3f81e4f96a663bcfc7
kongming <kongming434@gamil.com>
2025-06-20 00:57:44 +0700

    fix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -326,7 +326,7 @@ $ContactUs = [
                 loader.classList.add('opacity-0', 'pointer-events-none');
                 mainContent.classList.remove('opacity-0');
                 mainContent.classList.add('opacity-100'); // optional: smooth fade-in
-            }, 3000); // 3000ms = 3 seconds
+            }, 1000); // 3000ms = 3 seconds
 
         });
     </script>
